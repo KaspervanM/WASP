6a36dec7591ae86f3b87ec1b33200d9e6fd7a303
BuildTools <unconfigured@null.spigotmc.org>
2021-03-18 11:20:33 +0100

    first commit

---
+++ client/src/components/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="welcome">
+    <p id="title">WASP</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Welcome"
+});
+</script>
+
+<style scoped lang="scss">
+#title {
+  font-size: 10em;
+}
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Welcome></Welcome>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Welcome from "@/components/Welcome.vue";
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    Welcome
+  }
+});
+</script>
