ebbce5fd1d8b11557fdce89e33b87f4e332d0d4c
Evan Pacini <evan.pacini@gmail.com>
2021-04-06 22:18:20 +0200

    create and read tasks

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,26 +1,54 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const port = 3000;
-app.get('/users', (req, res) => {
-  return res.send('GET HTTP method on user resource');
-});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let tasks = {
+  1: {
+    id: '1',
+    title: 'Very important task n.1',
+    description: 'Description of task n.1',
+  },
+  2: {
+    id: '2',
+    title: 'Very important task n.2',
+    description: 'Description of task n.2',
+  },
+};
+
+app.get('/task', (req, res) => {
+  return res.send(Object.values(tasks));
+}); //Read all tasks
  
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
-});
+app.get('/task/:tId', (req, res) => {
+  return res.send(tasks[req.params.tId]);
+}); //Read one task
+
+app.post('/task', (req, res) => {
+  const id = uuidv4();
+  const task = {
+    id,
+    title: req.body.title,
+    description: req.body.description,
+  };
  
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
-});
+  tasks[id] = task;
  
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`,
-  );
-});
+  return res.send(task);
+}); //Add one task
+ 
+app.put('/task/:tId', (req, res) => {
+  return res.send(`PUT HTTP method on task/${req.params.tId} resource`,);
+}); //Update one task
+ 
+app.delete('/task/:tId', (req, res) => {
+  return res.send(`DELETE HTTP method on task/${req.params.tId} resource`,);
+}); //Delete one task
+
 app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
+  return console.log(`Server is listening on ${port}`);
 });
