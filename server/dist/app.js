51d843741613592357d17a941cb640b4e5e9fefe
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 18:30:23 +0200

    A task is now returned when it is requested.

--- server/dist/app.js
+++ server/dist/app.js
@@ -9,6 +9,9 @@ const port = 3000;
 app.get("/", (req, res) => {
     res.send("The sedulous hyena ate the antelope!");
 });
+app.get("/taskplease", (req, res) => {
+    res.send("Hey, it's me. The server.");
+});
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
 });
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,15 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+
 app.get("/", (req, res) => {
   res.send("The sedulous hyena ate the antelope!");
 });
+
+app.get("/taskplease", (req, res) => {
+  res.send("Hey, it's me. The server.");
+});
+
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
