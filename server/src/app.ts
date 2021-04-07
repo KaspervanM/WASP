0d1ac4e3948f6d7210a8bcaf0c67342b198c678b
Evan Pacini <65279226+evanpacini@users.noreply.github.com>
2021-04-07 13:12:24 +0200

    UpperCamelCase Task interface
    Co-authored-by: Kasper van Maasdam <kaspervanm@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -46,7 +46,7 @@ app.post("/task", (req, res) => {
 }); //Add one task (JSON: request contains title, description and code, response contains id, title, description and code)
 
 app.put("/task", (req, res) => {
-  const upTask: task = {
+  const upTask: Task = {
     id: req.body.id,
     title: req.body.title,
     description: req.body.description,
