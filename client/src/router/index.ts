bbae6a064d9c27da4f502dd2f9e6be4ab82e2c6e
Evan Pacini <evan.pacini@gmail.com>
2021-04-08 01:42:18 +0200

    Initial AddTask commit

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
+import AddTask from "../views/AddTask.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "Dashboard",
     component: Dashboard
+  },
+  {
+    path: "/addtask",
+    name: "Add Task",
+    component: AddTask
   },
   {
     path: "/about",
