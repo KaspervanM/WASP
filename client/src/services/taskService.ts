78dbd474fb46b2c952da1e1b603bf1c19a24d216
Evan Pacini <evan.pacini@gmail.com>
2021-04-13 00:27:17 +0200

    set AxiosResponse type + simplify getTasks

--- client/src/services/taskService.ts
+++ client/src/services/taskService.ts
@@ -5,7 +5,7 @@
 // Access functions:
 //   taskService.getTask();
 
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const serverURL = "http://localhost:3000/";
 
@@ -19,28 +19,35 @@ type TaskList = { [id: string]: Task };
 
 const taskService = {
   async getTasks(): Promise<TaskList> {
-    const res = await axios.get<TaskList>(serverURL + "task");
-    const tasks: TaskList = res.data;
-    return tasks;
+    const res: AxiosResponse = await axios.get<TaskList>(serverURL + "task");
+    return res.data;
   },
   async getTask(taskId: string): Promise<Task> {
-    const res = await axios.get<Task>(serverURL + "task/" + taskId);
+    const res: AxiosResponse = await axios.get<Task>(
+      serverURL + "task/" + taskId
+    );
     return res.data;
   },
   async idValidator(taskId: string): Promise<boolean> {
-    const res = await axios.get<Task>(serverURL + "task/" + taskId);
+    const res: AxiosResponse = await axios.get<Task>(
+      serverURL + "task/" + taskId
+    );
     return typeof res.data.title !== "undefined";
   },
   async addTask(task: Task): Promise<string> {
-    const res = await axios.post<Task>(serverURL + "task", task, {
-      headers: {
-        "Content-Type": "application/json"
+    const res: AxiosResponse = await axios.post<Task>(
+      serverURL + "task",
+      task,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
       }
-    });
+    );
     return res.data.id;
   },
   async updateTask(task: Task): Promise<string> {
-    const res = await axios.put<Task>(serverURL + "task", task, {
+    const res: AxiosResponse = await axios.put<Task>(serverURL + "task", task, {
       headers: {
         "Content-Type": "application/json"
       }
@@ -48,7 +55,9 @@ const taskService = {
     return res.data.id;
   },
   async deleteTask(taskId: string): Promise<string> {
-    const res = await axios.delete<Task>(serverURL + "task/" + taskId);
+    const res: AxiosResponse = await axios.delete<Task>(
+      serverURL + "task/" + taskId
+    );
     return res.data.id;
   }
 };
