c3eb3a2030e3c0d6ce858fd23ee2e3525f947a53
Evan Pacini <evan.pacini@gmail.com>
2021-05-09 17:58:05 +0200

    taskService error handling

--- client/src/services/taskService.ts
+++ client/src/services/taskService.ts
@@ -28,72 +28,122 @@ interface Task {
 }
 type TaskList = { [id: string]: Task };
 
+/* Handle Axios errors */
+function errorHandler(error: any) {
+  if (error.response) {
+    console.error(error.response.data);
+    console.error(error.response.status);
+    console.error(error.response.headers);
+    return error.response;
+  } else if (error.request) {
+    console.error(error.request);
+    return error.request;
+  } else {
+    console.error("Error", error.message);
+    return error.message;
+  }
+}
+
 const taskService = {
   async getTasks(): Promise<TaskList> {
-    const res: AxiosResponse = await axios.get<TaskList>(serverURL + "task");
-    return res.data;
+    try {
+      const res: AxiosResponse = await axios.get<TaskList>(serverURL + "task");
+      return res.data;
+    } catch (error) {
+      return Promise.reject(errorHandler(error));
+    }
   },
   async getTask(id: string): Promise<Task> {
-    const res: AxiosResponse = await axios.get<Task>(serverURL + "task/" + id);
-    return res.data;
+    try {
+      const res: AxiosResponse = await axios.get<Task>(
+        serverURL + "task/" + id
+      );
+      return res.data;
+    } catch (error) {
+      return Promise.reject(errorHandler(error));
+    }
   },
   async getSubtask(id: string): Promise<[SubTask, string]> {
-    const res: AxiosResponse = await axios.get<[SubTask, string]>(
-      serverURL + "task/request-subtask/" + id
-    );
-    return res.data;
+    try {
+      const res: AxiosResponse = await axios.get<[SubTask, string]>(
+        serverURL + "task/request-subtask/" + id
+      );
+      return res.data;
+    } catch (error) {
+      return Promise.reject(errorHandler(error));
+    }
   },
   async returnSubresult(
     id: string,
     subtask: SubTask,
     result: string | number | Array<string | number>
   ): Promise<boolean> {
-    const res: AxiosResponse = await axios.post<boolean>(
-      serverURL + "task/return-subresult/",
-      { id, subtask, result },
-      {
-        headers: {
-          "Content-Type": "application/json"
+    try {
+      const res: AxiosResponse = await axios.post<boolean>(
+        serverURL + "task/return-subresult/",
+        { id, subtask, result },
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
         }
-      }
-    );
-    return res.data;
+      );
+      return res.data;
+    } catch (error) {
+      return Promise.reject(errorHandler(error));
+    }
   },
   async getTaskProgress(id: string): Promise<[TaskProgress, number]> {
-    const res = await axios.get<[TaskProgress, number]>(
-      serverURL + "task/progress/" + id
-    );
-    return res.data;
+    try {
+      const res = await axios.get<[TaskProgress, number]>(
+        serverURL + "task/progress/" + id
+      );
+      return res.data;
+    } catch (error) {
+      return Promise.reject(errorHandler(error));
+    }
   },
   async addTask(task: Task): Promise<string> {
-    const res: AxiosResponse = await axios.post<Task>(
-      serverURL + "task",
-      task,
-      {
-        headers: {
-          "Content-Type": "application/json"
+    try {
+      const res: AxiosResponse = await axios.post<Task>(
+        serverURL + "task",
+        task,
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
         }
-      }
-    );
-    return res.data;
+      );
+      return res.data;
+    } catch (error) {
+      return Promise.reject(errorHandler(error));
+    }
   },
   async updateTask(task: Task, reset: boolean): Promise<string> {
-    const res: AxiosResponse = await axios.put<Task>(
-      serverURL + "task",
-      { task, reset },
-      {
-        headers: {
-          "Content-Type": "application/json"
+    try {
+      const res: AxiosResponse = await axios.put<Task>(
+        serverURL + "task",
+        { task, reset },
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
         }
-      }
-    );
-    return res.data.id;
+      );
+      return res.data.id;
+    } catch (error) {
+      return Promise.reject(errorHandler(error));
+    }
   },
   async deleteTask(id: string): Promise<string> {
-    const res: AxiosResponse = await axios.delete<Task>(
-      serverURL + "task/" + id
-    );
-    return res.data.id;
+    try {
+      const res: AxiosResponse = await axios.delete<Task>(
+        serverURL + "task/" + id
+      );
+      return res.data.id;
+    } catch (error) {
+      return Promise.reject(errorHandler(error));
+    }
   }
 };
 export default taskService;
