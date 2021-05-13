c1124e095c617ee5cb084790f1fe14550d4836a8
KaspervanM <kaspervanm@gmail.com>
2021-05-13 16:45:48 +0200

    Better password prompt.

--- client/src/views/Results.vue
+++ client/src/views/Results.vue
@@ -6,9 +6,35 @@
       <b-button variant="outline-success" @click="returnResults"
         >Give me the results!</b-button
       >
-      <b-button variant="outline-danger" @click="leaveResults"
+      <b-button variant="outline-danger" @click="leaveResultsPage"
         >Leave this task</b-button
       >
+      <b-modal
+        id="password-prompt"
+        v-model="showPrompt"
+        ref="modal"
+        title="Enter Password"
+        @show="resetModal"
+        @hidden="resetModal"
+        @ok="handleOk"
+      >
+        <form ref="form" @submit.stop.prevent="onSubmit">
+          <b-form-group
+            label="Password"
+            label-for="password-input"
+            invalid-feedback="Password is required"
+            :state="nameState"
+          >
+            <b-form-input
+              id="password-input"
+              type="password"
+              v-model="password"
+              :state="nameState"
+              required
+            ></b-form-input>
+          </b-form-group>
+        </form>
+      </b-modal>
     </div>
   </div>
 </template>
@@ -19,6 +45,13 @@ import taskService from "@/services/taskService";
 
 export default Vue.extend({
   name: "Results",
+  data(): { showPrompt: boolean; password: string; nameState: any } {
+    return {
+      showPrompt: false,
+      password: "",
+      nameState: null
+    };
+  },
   props: { taskId: String },
   created() {
     if (!this.$cookies.isKey("TaskId")) {
@@ -27,53 +60,46 @@ export default Vue.extend({
   },
   methods: {
     returnResults: function (): void {
-      this.initiateDownload()
-        .then(() => {
-          console.log("done");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      this.showPrompt = true;
     },
-    initiateDownload: function (): Promise<string> {
-      const id = this.taskId;
-      const promptPassword = this.promptPassword;
-      return new Promise(function (resolve, reject) {
-        promptPassword().then((pass) => {
-          taskService
-            .downloadResult(id, pass)
-            .then((response) => {
-              const blob = new Blob([JSON.stringify(response.data)], {
-                type: "application/plain"
-              });
-              const link = document.createElement("a");
-              link.href = URL.createObjectURL(blob);
-              link.download = id.toString() + ".json";
-              link.click();
-              URL.revokeObjectURL(link.href);
-              resolve("success");
-            })
-            .catch(() => {
-              console.error;
-              reject("error getting results");
-            });
-        });
-      });
-    },
-    promptPassword: async function (): Promise<string> {
-      const privateTask: boolean = (await taskService.getTask(this.taskId))
-        .config["PRIVATE"];
-      console.log(await taskService.getTask(this.taskId));
-      return new Promise(function (resolve) {
-        if (privateTask) {
-          resolve(prompt("Enter the password"));
-          return;
-        }
-        resolve("");
+    onSubmit: function () {
+      if (!this.checkFormValidity()) {
         return;
-      });
+      }
+      const id = this.taskId;
+      const pass = this.password;
+      taskService
+        .downloadResult(id, pass)
+        .then((response) => {
+          const blob = new Blob([JSON.stringify(response.data)], {
+            type: "application/plain"
+          });
+          const link = document.createElement("a");
+          link.href = URL.createObjectURL(blob);
+          link.download = id.toString() + ".json";
+          link.click();
+          URL.revokeObjectURL(link.href);
+        })
+        .catch(() => {
+          console.error;
+        });
     },
-    leaveResults: function () {
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity();
+      this.nameState = valid;
+      return valid;
+    },
+    resetModal() {
+      this.password = "";
+      this.nameState = null;
+    },
+    handleOk(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault();
+      // Trigger submit handler
+      this.onSubmit();
+    },
+    leaveResultsPage: function () {
       this.$emit("remove-cookies");
       this.$router.push("/");
     }
