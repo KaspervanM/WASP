82a9cea802b4a258b1cb4718b55658fe2d8d3fc2
KaspervanM <kaspervanm@gmail.com>
2021-05-14 14:02:47 +0200

    Removal of unnecessary code and redirect to results page.

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -7,6 +7,7 @@
           :taskId="taskId"
           v-if="taskId.length > 0"
           :key="NaN"
+          @task-done="showResults"
         ></TheProgressBar>
         <p>Congratulations: {{ taskId }} is now running!</p>
         <b-button
@@ -66,11 +67,9 @@ export default Vue.extend({
   name: "Dashboard",
   components: { TheProgressBar },
   props: { taskId: String },
-  data(): { runTaskId: string; max: number; value: number } {
+  data(): { runTaskId: string } {
     return {
-      runTaskId: "",
-      max: 100,
-      value: 60.0
+      runTaskId: ""
     };
   },
   computed: {
@@ -86,6 +85,9 @@ export default Vue.extend({
       if (this.taskIdState) {
         this.$router.push("/id/" + this.runTaskId);
       }
+    },
+    showResults: function (): void {
+      this.$router.push("/results");
     }
   }
 });
