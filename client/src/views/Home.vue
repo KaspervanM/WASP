5a5467f932597079cc3759b49c3fc1503acecb72
Kasper van Maasdam <kaspervanm@gmail.com>
2021-05-26 14:32:40 +0200

    Added home page
    Please test this.

---
+++ client/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div class="parallax image1"></div>
+    <p class="header1" id="title">Home</p>
+  </div>
+</template>
+
+<script lang="typescript">
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  width: 100%;
+  height: auto;
+}
+.parallax {
+	min-height: 500px;
+	background-attachment: fixed;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+.image1 {
+	background-image: url("../assets/hide-the-pain-harold-why-you-should-not-use-stock-photos.jpg");
+	image-orientation: from-image;
+}
+#title {
+  float: left;
+  display: inline-flex;
+}
