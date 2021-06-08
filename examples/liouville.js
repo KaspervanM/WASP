8efac7f60646e6e7daf5971e1a73e8d7204ee1f9
Evan Pacini <evan.pacini@gmail.com>
2021-06-08 23:53:40 +0200

    add liouville's constant computation

---
+++ examples/liouville.js
@@ -0,0 +1,23 @@
+/*
+ * Liouville's constant computation in Javascript using the BigInt type
+ * No post-processing needed, just "0." in front of the results (eg. 110001 => 0.110001)
+ */
+"use strict";
+const DP = 10n ** BigInt(1e5) + 64n; // Decimal precision
+
+function fact(n) {
+  return n > 1 ? n * fact(n - 1n) : 1n;
+}
+
+function main(start, end) {
+  const bigEnd = BigInt(end);
+  let sum = 0n;
+  let k = BigInt(start);
+
+  for (;;) {
+    sum += DP / 10n ** fact(k);
+    k += 1n;
+    if (k > bigEnd) break;
+  }
+  return sum.toString();
+}
