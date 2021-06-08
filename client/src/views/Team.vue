10aa2c80f73abda3cb00f600d0e1b4d3843fb259
KaspervanM <kaspervanm@gmail.com>
2021-06-08 21:05:34 +0200

    Id's were supposed to be classes as they were not unique identifiers, the flex-flow was supposed to be column and the width of b-container was limited.

--- client/src/views/Team.vue
+++ client/src/views/Team.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="team">
-    <p class="header1" id="title">Our Team</p>
+    <p class="header1 title">Our Team</p>
     <b-container>
-      <b-row id="row">
+      <b-row class="row">
         <b-img
           rounded
           :src="require('@/assets/team/kasper.jpg')"
           alt="Kasper van Maasdam"
           fluid-grow
-          id="img"
+          class="img"
         ></b-img>
-        <b-col id="info">
-          <p class="header2" id="title">Kasper van Maasdam</p>
-          <p id="text">
+        <b-col class="info">
+          <p class="header2 title">Kasper van Maasdam</p>
+          <p class="text">
             Kasper van Maasdam is de Project Manager/Scrum Master. Hij is
             verantwoordelijk voor het eindproduct en voor een soepel verloop van
             het ontwikkelingsproces. Ook is het de taak van Kasper dat geen van
@@ -21,33 +21,33 @@
           </p>
         </b-col>
       </b-row>
-      <b-row id="row">
+      <b-row class="row">
         <b-img
           rounded
           :src="require('@/assets/team/evan.jpg')"
           alt="Evan Pacini"
           fluid-grow
-          id="img"
+          class="img"
         ></b-img>
-        <b-col id="info">
-          <p class="header2" id="title">Evan Pacini</p>
-          <p id="text">
+        <b-col class="info">
+          <p class="header2 title">Evan Pacini</p>
+          <p class="text">
             Evan Pacini is een teamlid. Zijn taak is om het project voort te
             laten gaan, door Agile mee te werken met zijn mede teamleden.
           </p>
         </b-col>
       </b-row>
-      <b-row id="row">
+      <b-row class="row">
         <b-img
           rounded
           :src="require('@/assets/team/luke.jpg')"
           alt="Luke Kuijpers"
           fluid-grow
-          id="img"
+          class="img"
         ></b-img>
-        <b-col id="info">
-          <p class="header2" id="title">Luke Kuijpers</p>
-          <p id="text">
+        <b-col class="info">
+          <p class="header2 title">Luke Kuijpers</p>
+          <p class="text">
             Luke Kuijpers is ook een teamlid. Hij werkt samen met Kasper en Evan
             om dit een episch product te maken. 😎 Alhoewel hij minder ervaring
             heeft met programmeren zal hij zijn best doen om zijn steentje bij
@@ -55,17 +55,17 @@
           </p>
         </b-col>
       </b-row>
-      <b-row id="row">
+      <b-row class="row">
         <b-img
           rounded
           :src="require('@/assets/team/dion.jpg')"
           alt="Dion van Dam"
           fluid-grow
-          id="img"
+          class="img"
         ></b-img>
-        <b-col id="info">
-          <p class="header2" id="title">Dion van Dam</p>
-          <p id="text">
+        <b-col class="info">
+          <p class="header2 title">Dion van Dam</p>
+          <p class="text">
             Dion van Dam is de opdrachtgever en komt geregeld in contact met de
             rest van het team om het verloop van het project te bespreken. Hij
             is Scrum Master en is in dienst bij Atos. Hij doet op dit moment een
@@ -81,23 +81,27 @@
   width: 100%;
   height: auto;
   display: flex;
-  flex-flow: row wrap;
+  flex-flow: column;
 }
-#title {
+.title {
   float: left;
   display: inline-flex;
 }
-#row {
+.container {
+  max-width: none;
+  margin: 0;
+}
+.row {
   display: flex;
   flex-flow: row nowrap;
-  margin: 10px;
-  #img {
+  margin: 20px;
+  .img {
     max-width: 15vw;
   }
-  #info {
+  .info {
     display: flex;
     flex-flow: column wrap;
-    #text {
+    .text {
       text-align: left;
     }
   }
