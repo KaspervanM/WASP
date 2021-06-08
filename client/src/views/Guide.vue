1b6c5ccdba0177837c12dbcd08c641141a5c1232
Kasper van Maasdam <kaspervanm@gmail.com>
2021-06-08 13:59:17 +0200

    Table of contents

--- client/src/views/Guide.vue
+++ client/src/views/Guide.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="guide">
     <p class="header1">Guide</h1>
+    <p class="header2">Table of contents</h1>
+    <ol>
+      <li><a href="#intro">Introduction</a></li>
+    </ol>
+    <p class="header2" id="intro">Introduction</h1>
+    <p>The ...</p>
   </div>
 </template>
