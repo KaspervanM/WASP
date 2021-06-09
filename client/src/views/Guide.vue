d427eb50131e67a1ffebc3308ffd9932a7f3688e
KaspervanM <kaspervanm@gmail.com>
2021-06-09 14:50:15 +0200

    Guide
    Co-authored-by: Evan Pacini <evanpacini@users.noreply.github.com>

--- client/src/views/Guide.vue
+++ client/src/views/Guide.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="guide">
-    <h1>This is a guide page</h1>
+    <p class="header1">Guide</p>
+    <p class="header2">Types of processes</p>
+    <p class="header2">Creating a task (for the Requester)</p>
+    <p>
+      This is a guide for those that want to upload their code for others to
+      help with.
+    </p>
+    <p class="header3">Uploading code</p>
+    <p class="header3">Acceptable code</p>
+    <p class="header3">Configuration settings</p>
+    <p class="header3">What to take into account</p>
+    <p class="header2">Contributing to a task (for the Helper)</p>
+    <p>
+      This is a guide for those that want to contribute to a task for other
+      Requesters.
+    </p>
   </div>
 </template>
+<style lang="scss" scoped>
+.guide {
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-flow: column;
+  text-align: left;
+}
+</style>
